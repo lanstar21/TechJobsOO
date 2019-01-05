a7b5bafedccfd26a77bf971655b440cfe85c6577
lanstar21 <lanstar.804@gmail.com>
2019-01-05 17:57:25 -0500

    finished assignment

---
+++ src/TechJobs/ViewModels/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechJobs.Models;
+
+namespace TechJobs.ViewModels
+{
+    public class BaseViewModel
+    {
+        // View title
+        public string Title { get; set; } = "";
+
+        // All columns, for display
+        public List<JobFieldType> Columns { get; set; }
+
+        public JobFieldType Column { get; set; }
+    }
+}
